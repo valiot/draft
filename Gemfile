4ea9ae91512a5405004d538ba05a86de12cb65fc
Adrian Rangel <adrian.rangel@gmail.com>
2016-05-06 17:49:02 -0500

    google auth

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'omniauth-facebook'
 gem 'omniauth-github'
 gem 'omniauth-identity'
+gem 'omniauth-google-oauth2'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '>= 5.0.0.beta4', '< 5.1'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user:email'
   provider :identity, fields: [:name, :email],
            on_failed_registration: lambda { |env| IdentitiesController.action(:new).call(env) }
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
 end
