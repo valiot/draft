471de45fc4a858387bdddcfbf1ee40b41a09e7ed
Adrian Rangel <adrian.rangel@gmail.com>
2016-05-05 19:08:20 -0500

    use dotenv

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '239426226415375', '45da84a3381370f03fa7d748327c1767',
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
            scope: [:email, :public_profile], secure_image_url: true, image_size: :large
 end
