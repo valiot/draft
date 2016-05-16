26ead2616540a1c93b9bc8883daba372742412f8
Balbina <balbinasantana@gmail.com>
2016-05-16 14:03:47 -0500

    Header logic fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def login_header
+    if current_user
+      raw '<li class="nav-item">
+        <a class="nav-link" href="/user/edit">' + current_user&.name + '</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/logout">Logout</a>
+      </li>'
+    else
+      raw '<li class="nav-item">
+        <a class="nav-link" href="/login">Login</a>
+      </li>'
+    end
+  end
 end
