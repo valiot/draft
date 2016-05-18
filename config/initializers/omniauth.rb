4ad4f22d32a6dcfa9f6f5b563320959366dd7a92
Adrian Rangel <adrian.rangel@gmail.com>
2016-05-18 15:53:03 -0500

    bigger fb profile image

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-           scope: [:email, :public_profile], secure_image_url: true, image_size: :large
+           scope: [:email, :public_profile], secure_image_url: true,
+           image_size: { width: 400, height: 400 }
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user:email'
   provider :identity, fields: [:name, :email],
                       on_failed_registration: lambda { |env|
