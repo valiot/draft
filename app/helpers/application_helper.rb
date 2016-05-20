6ac1c486a9f609cdf56972ef116c755938149da6
Adrian Rangel <adrian.rangel@gmail.com>
2016-05-19 22:57:07 -0500

    improve navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def logout_header
-    return "<a class='btn btn-danger-outline' href='/logout'>Logout</a>".html_safe if current_user
+    return "<a class='btn btn-danger-outline nav-item' href='/logout'>Logout</a>".html_safe if current_user
   end
 
   def pending_invitations_header
-    link_to 'Invitaciones pendientes', invitations_path, class: 'btn btn-primary-outline' if current_user&.pending_invitations?
+    link_to 'Invitaciones pendientes', invitations_path, class: 'btn btn-primary-outline nav-item' if current_user&.pending_invitations?
   end
 end
