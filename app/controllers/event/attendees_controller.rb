0ff1562ef00908b1c5428f656214904b01ee540c
Adrian Rangel <adrian.rangel@gmail.com>
2016-06-03 16:21:52 -0500

    use event_main_url

--- app/controllers/event/attendees_controller.rb
+++ app/controllers/event/attendees_controller.rb
@@ -6,7 +6,7 @@ class Event::AttendeesController < ApplicationController
     if Attendee.create(user: current_user, event: current_event)
       session[:user_id] = nil
       flash[:notice] = 'Tu check-in ha quedado registrado.'
-      render js: "var win = window.open('http://localhost:3000/disrupting-xignux/', '_blank');
+      render js: "var win = window.open('#{event_main_url}', '_blank');
   win.focus();window.setTimeout(function(){this.close();},1000)"
     else
       render event_checkin_path
