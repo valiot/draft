6922aceaa0007da26553c44c5f222518da1e80c2
Adrian Rangel <adrian.rangel@gmail.com>
2016-09-19 12:09:19 -0500

    set btn outline v4 bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def logout_header
-    return link_to 'Log out', logout_path, class: 'btn btn-danger-outline nav-item' if current_user
+    return link_to 'Log out', logout_path, class: 'btn btn-outline-danger nav-item' if current_user
   end
 
   def pending_invitations_header
-    link_to 'Invitaciones pendientes', invitations_path, class: 'btn btn-primary-outline nav-item' if current_user&.pending_invitations?
+    link_to 'Invitaciones pendientes', invitations_path, class: 'btn btn-outline-primary nav-item' if current_user&.pending_invitations?
   end
 end
