26d3c65524eb51f6421fa0b2d1de03eb65333896
Adrian Rangel <adrian.rangel@gmail.com>
2016-09-30 17:02:53 -0500

    seed for katcon

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,42 +11,41 @@
 #   finishes_at: "2016-06-2 20:00:00"
 # )
 
-# e = Event.find_by(slug: 'disrupting-katcon’)
-#
-#
-# Team.create(
-#   name: “SOLDADURA 1",
-#   event_id: e.id,
-#   profiles: { "Hacker"=>2, "Maker"=>1, "Random"=>1, “IFI”=>1 }
-# )
-#
-#
-# Team.create(
-#   name: “SOLDADURA 2”,
-#   event_id: e.id,
-#   profiles: { "Hacker"=>2, “Data”=>1, “Non IT”=>1, “Random”=>1 }
-# )
-#
-# Team.create(
-#   name: “ANNULUS 1“,
-#   event_id: e.id,
-#   profiles: { "Hacker"=>1, “Industrial Designer”=>1, “Math”=>1, "Random"=>1, “Maker”=>1}
-# )
-#
-# Team.create(
-#   name: “ANNULUS 2",
-#   event_id: e.id,
-#   profiles: { "Hacker"=>1, "Maker"=>1, "Industrial Designer"=>1, “Random”=>1, “Math”=>1 }
-# )
-#
-# Team.create(
-#   name: “CICLO 1”,
-#   event_id: e.id,
-#   profiles: { "Hacker"=>1, "Maker"=>1, “Non IT”=>1, “Data”=>1, “Random”=>1 }
-# )
-#
-# Team.create(
-#   name: “CICLO 2”,
-#   event_id: e.id,
-#   profiles: { "Hacker"=>2, "Maker"=>1, “Non IT”=>1, “Random”=>1}
-# )
+e = Event.find_by(slug: 'disrupting-katcon')
+
+Team.create(
+  name: "SOLDADURA 1",
+  event_id: e.id,
+  profiles: { "Hacker"=>2, "Maker"=>1, "Random"=>1, "IFI"=>1 }
+)
+
+
+Team.create(
+  name: "SOLDADURA 2",
+  event_id: e.id,
+  profiles: { "Hacker"=>2, "Data"=>1, "Non IT"=>1, "Random"=>1 }
+)
+
+Team.create(
+  name: "ANNULUS 1",
+  event_id: e.id,
+  profiles: { "Hacker"=>1, "Industrial Designer"=>1, "Math"=>1, "Random"=>1, "Maker"=>1}
+)
+
+Team.create(
+  name: "ANNULUS 2",
+  event_id: e.id,
+  profiles: { "Hacker"=>1, "Maker"=>1, "Industrial Designer"=>1, "Random"=>1, "Math"=>1 }
+)
+
+Team.create(
+  name: "CICLO 1",
+  event_id: e.id,
+  profiles: { "Hacker"=>1, "Maker"=>1, "Non IT"=>1, "Data"=>1, "Random"=>1 }
+)
+
+Team.create(
+  name: "CICLO 2",
+  event_id: e.id,
+  profiles: { "Hacker"=>2, "Maker"=>1, "Non IT"=>1, "Random"=>1}
+)
