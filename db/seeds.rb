6af63ca80b68ef3cda44a255d81f76ba0f23d100
Adrian Rangel <adrian.rangel@gmail.com>
2016-09-30 19:02:21 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,39 +13,39 @@
 
 e = Event.find_by(slug: 'disrupting-katcon')
 
-Team.create(
+Team.find(7).update(
   name: "SOLDADURA 1",
   event_id: e.id,
-  profiles: { "Hacker"=>2, "Maker"=>1, "Random"=>1, "IFI"=>1 }
+  profiles: { "Hacker"=>2, "Random"=>1, "IFI"=>1 }
 )
 
 
-Team.create(
+Team.find(8).update(
   name: "SOLDADURA 2",
   event_id: e.id,
-  profiles: { "Hacker"=>2, "Data"=>1, "Non IT"=>1, "Random"=>1 }
+  profiles: { "Hacker"=>2, "Data"=>1, "Random"=>1 }
 )
 
-Team.create(
+Team.find(9).update(
   name: "ANNULUS 1",
   event_id: e.id,
-  profiles: { "Hacker"=>1, "Industrial Designer"=>1, "Math"=>1, "Random"=>1, "Maker"=>1}
+  profiles: { "Hacker"=>1, "Industrial Designer"=>1, "Math"=>1, "Random"=>1}
 )
 
-Team.create(
+Team.find(10).update(
   name: "ANNULUS 2",
   event_id: e.id,
-  profiles: { "Hacker"=>1, "Maker"=>1, "Industrial Designer"=>1, "Random"=>1, "Math"=>1 }
+  profiles: { "Hacker"=>1, "Industrial Designer"=>1, "Random"=>1, "Math"=>1 }
 )
 
-Team.create(
+Team.find(11).update(
   name: "CICLO 1",
   event_id: e.id,
-  profiles: { "Hacker"=>1, "Maker"=>1, "Non IT"=>1, "Data"=>1, "Random"=>1 }
+  profiles: { "Hacker"=>1, "Non IT"=>1, "Data"=>1, "Random"=>1 }
 )
 
-Team.create(
+Team.find(12).update(
   name: "CICLO 2",
   event_id: e.id,
-  profiles: { "Hacker"=>2, "Maker"=>1, "Non IT"=>1, "Random"=>1}
+  profiles: { "Hacker"=>1, "Maker"=>1, "Non IT"=>1, "Random"=>1}
 )
