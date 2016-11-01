21b58994f84b6c139a206840459edbb6caf70b4a
Adrian Rangel <adrian.rangel@gmail.com>
2016-11-01 08:35:59 -0600

    invited user dashboard

---
+++ app/dashboards/invited_user_dashboard.rb
@@ -0,0 +1,69 @@
+require "administrate/base_dashboard"
+
+class InvitedUserDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    profile: Field::BelongsTo,
+    invitations: Field::HasMany,
+    attendees: Field::HasMany,
+    name: Field::String,
+    email: Field::String,
+    image_url: Field::String,
+    role: Field::Enum,
+    shirt_size: Field::Enum,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :name,
+    :profile,
+    :shirt_size,
+    :id
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :profile,
+    :invitations,
+    :attendees,
+    :name,
+    :email,
+    :image_url,
+    :role,
+    :shirt_size,
+    :created_at,
+    :updated_at,
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :profile,
+    :name,
+    :email,
+    :image_url,
+    :role,
+    :shirt_size,
+  ].freeze
+
+  # Overwrite this method to customize how users are displayed
+  # across all pages of the admin dashboard.
+  #
+  def display_resource(user)
+    user.name
+  end
+end
