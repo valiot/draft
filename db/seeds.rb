091d95aa3349cca85cf5ddaccfeb7bf98907c8ef
Adrian Rangel <adrian.rangel@gmail.com>
2016-11-04 16:26:06 -0600

    seeds for farmacias

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,40 @@
 # The profile hash must not include the leader of the team.
 
-# e = Event.find_by(slug: 'disrupting-katcon')
-#
-# Team.find(7).update(
-#   name: 'SOLDADURA 1',
-#   event_id: e.id,
-#   profiles: { 'Hacker'=>2, 'Random'=>1, 'IFI'=>1 }
-# )
-#
-#
-# Team.find(8).update(
-#   name: 'SOLDADURA 2',
-#   event_id: e.id,
-#   profiles: { 'Hacker'=>2, 'Data'=>1, 'Random'=>1 }
-# )
-#
-# Team.find(9).update(
-#   name: 'ANNULUS 1',
-#   event_id: e.id,
-#   profiles: { 'Hacker'=>1, 'Industrial Designer'=>1, 'Math'=>1, 'Random'=>1}
-# )
-#
-# Team.find(10).update(
-#   name: 'ANNULUS 2',
-#   event_id: e.id,
-#   profiles: { 'Hacker'=>1, 'Industrial Designer'=>1, 'Random'=>1, 'Math'=>1 }
-# )
-#
-# Team.find(11).update(
+e = Event.find_by(slug: 'disrupting-farmacias-del-ahorro')
+
+Team.create(
+  name: 'Servicio Orientación Medica 1',
+  event_id: e.id,
+  profiles: { 'Data' => 2, 'Designer' => 1, 'Hacker' => 1, 'Rockstar' => 1 }
+)
+
+
+Team.create(
+  name: 'Servicio Orientación Medica 2',
+  event_id: e.id,
+  profiles: { 'Hacker' => 2, 'Data' => 1, 'Designer' => 1, 'Rockstar' => 1 }
+)
+
+Team.create(
+  name: 'Servicio a Domicilio 1',
+  event_id: e.id,
+  profiles: { 'Hacker' => 1, 'Non IT' => 1, 'Data' => 1, 'Rockstar' => 1, 'Designer' => 1}
+)
+
+Team.create(
+  name: 'Servicio a Domicilio 2',
+  event_id: e.id,
+  profiles: { 'Hacker' => 1, 'Maker' => 1, 'Data' => 1, 'Rockstar' => 1, 'Designer' => 1}
+)
+
+# Team.create(
 #   name: 'CICLO 1',
 #   event_id: e.id,
-#   profiles: { 'Hacker'=>1, 'Non IT'=>1, 'Data'=>1, 'Random'=>1 }
+#   profiles: { 'Hacker' => 1, 'Non IT' => 1, 'Data' => 1, 'Rockstar' => 1 }
 # )
 #
-# Team.find(12).update(
+# Team.create(
 #   name: 'CICLO 2',
 #   event_id: e.id,
-#   profiles: { 'Hacker'=>1, 'Maker'=>1, 'Non IT'=>1, 'Random'=>1}
+#   profiles: { 'Hacker' => 1, 'Maker' => 1, 'Non IT' => 1, 'Rockstar' => 1}
 # )
