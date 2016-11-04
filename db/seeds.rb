18b6c5550b0053cf96e5ca74d58fd5598ad89099
Adrian Rangel <adrian.rangel@gmail.com>
2016-11-04 16:46:16 -0600

    update fam

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,29 +2,29 @@
 
 e = Event.find_by(slug: 'disrupting-farmacias-del-ahorro')
 
-Team.create(
+Team.find(13).update(
   name: 'Servicio Orientación Medica 1',
   event_id: e.id,
-  profiles: { 'Data' => 2, 'Designer' => 1, 'Hacker' => 1, 'Rockstar' => 1 }
+  profiles: { 'Data' => 1, 'Designer' => 1, 'Hacker' => 1, 'Rockstar' => 1 }
 )
 
 
-Team.create(
+Team.find(14).update(
   name: 'Servicio Orientación Medica 2',
   event_id: e.id,
-  profiles: { 'Hacker' => 2, 'Data' => 1, 'Designer' => 1, 'Rockstar' => 1 }
+  profiles: { 'Hacker' => 2, 'Data' => 1, 'Designer' => 1}
 )
 
-Team.create(
+Team.find(15).update(
   name: 'Servicio a Domicilio 1',
   event_id: e.id,
-  profiles: { 'Hacker' => 1, 'Non IT' => 1, 'Data' => 1, 'Rockstar' => 1, 'Designer' => 1}
+  profiles: { 'Hacker' => 1, 'Data' => 1, 'Rockstar' => 1, 'Designer' => 1}
 )
 
-Team.create(
+Team.find(16).update(
   name: 'Servicio a Domicilio 2',
   event_id: e.id,
-  profiles: { 'Hacker' => 1, 'Maker' => 1, 'Data' => 1, 'Rockstar' => 1, 'Designer' => 1}
+  profiles: { 'Maker' => 1, 'Data' => 1, 'Rockstar' => 1, 'Designer' => 1}
 )
 
 # Team.create(
