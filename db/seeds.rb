733d4daf55773dcdb7e53bfe948f8cd6543f4157
Adrian Rangel <adrian.rangel@gmail.com>
2016-11-24 11:32:47 -0600

    build teams of disrupting coflex

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,38 @@
 # The profile hash must not include the leader of the team.
 
-e = Event.find_by(slug: 'disrupting-farmacias-del-ahorro')
+e = Event.find_by(slug: 'disrupting-coflex')
 
-Team.find(13).update(
-  name: 'Servicio Orientación Medica 1',
+Team.create(
+  name: 'TENSIÓN 1',
   event_id: e.id,
-  profiles: { 'Data' => 1, 'Designer' => 1, 'Hacker' => 1, 'Rockstar' => 1 }
+  profiles: { 'Maker' => 1, 'Non IT' => 1, 'Industrial Designer' => 1, 'Rockstar' => 1 }
 )
 
-
-Team.find(14).update(
-  name: 'Servicio Orientación Medica 2',
+Team.create(
+  name: 'TENSIÓN 2',
   event_id: e.id,
-  profiles: { 'Hacker' => 2, 'Data' => 1, 'Designer' => 1}
+  profiles: { 'Maker' => 1, 'Non IT' => 1, 'Industrial Designer' => 1, 'Rockstar' => 1 }
 )
 
-Team.find(15).update(
-  name: 'Servicio a Domicilio 1',
+Team.create(
+  name: 'LOGISTICA 1',
   event_id: e.id,
-  profiles: { 'Hacker' => 1, 'Data' => 1, 'Rockstar' => 1, 'Designer' => 1}
+  profiles: { 'Hacker' => 1, 'Data' => 1, 'Designer' => 1, 'Rockstar' => 1 }
+)
+Team.create(
+  name: 'LOGISTICA 2',
+  event_id: e.id,
+  profiles: { 'Hacker' => 1, 'Data' => 1, 'Non IT' => 1, 'Rockstar' => 1 }
 )
 
-Team.find(16).update(
-  name: 'Servicio a Domicilio 2',
+Team.create(
+  name: 'SAPO 1',
   event_id: e.id,
-  profiles: { 'Maker' => 1, 'Data' => 1, 'Rockstar' => 1, 'Designer' => 1}
+  profiles: { 'Maker' => 2, 'Industrial Designer' => 1, 'Rockstar' => 1 }
 )
 
-# Team.create(
-#   name: 'CICLO 1',
-#   event_id: e.id,
-#   profiles: { 'Hacker' => 1, 'Non IT' => 1, 'Data' => 1, 'Rockstar' => 1 }
-# )
-#
-# Team.create(
-#   name: 'CICLO 2',
-#   event_id: e.id,
-#   profiles: { 'Hacker' => 1, 'Maker' => 1, 'Non IT' => 1, 'Rockstar' => 1}
-# )
+Team.create(
+  name: 'SAPO 2',
+  event_id: e.id,
+  profiles: { 'Maker' => 1, 'IFI' => 1, 'Industrial Designer' => 1, 'Rockstar' => 1 }
+)
