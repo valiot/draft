02d28fa0bdfafccdbd301de433a636b4c38b2f04
Adrian Rangel <adrian.rangel@gmail.com>
2016-11-24 09:14:54 -0600

    sort by created_at desc

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,5 +7,11 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     http_basic_authenticate_with name: ENV['LOGIN_NAME'], password: ENV['LOGIN_PASSWORD']
+    before_filter :default_params
+
+    def default_params
+      params[:order] ||= "created_at"
+      params[:direction] ||= "desc"
+    end
   end
 end
