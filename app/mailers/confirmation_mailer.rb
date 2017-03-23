3a46d60415bf19788912091347f8272c7d808edc
Adrian Rangel <adrian.rangel@gmail.com>
2017-03-23 12:10:49 -0600

    Confirmation email fixes

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -2,6 +2,6 @@ class ConfirmationMailer < ApplicationMailer
   def invitation_accepted(invitation)
     @user = invitation.user
     @event = invitation.event
-    mail(to: @user.email, subject: "🚀 #{@event.name.titleize} invitación confirmada")
+    mail(to: @user.email, subject: "🚀 #{@event.name} invitación confirmada")
   end
 end
