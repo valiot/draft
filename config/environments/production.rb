e32f83f6106dd1bef683d17c32504523c214d85d
Adrian Rangel <adrian.rangel@gmail.com>
2017-03-22 18:53:25 -0600

    change smtp port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
     password: ENV['SENDGRID_PASSWORD'],
     domain: 'disruptiveangels.com',
     address: 'smtp.sendgrid.net',
-    port: 465,
+    port: 587,
     authentication: :plain,
     enable_starttls_auto: true
   }
