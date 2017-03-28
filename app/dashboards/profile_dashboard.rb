281df63a7aa61fa54e90019404367c9daae0db78
Adrian Rangel <adrian.rangel@gmail.com>
2017-03-27 18:18:49 -0600

    fix create new profile

--- app/dashboards/profile_dashboard.rb
+++ app/dashboards/profile_dashboard.rb
@@ -42,7 +42,7 @@ class ProfileDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :users,
+    # :users,
     :name,
     :image
   ].freeze
