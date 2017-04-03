01ece349e6175a51ac6a42a43125240143226ab6
Adrian Rangel <adrian.rangel@gmail.com>
2017-04-03 01:26:07 -0500

    New Review View

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,11 +3,11 @@ class Question < ApplicationRecord
 
   def input(name)
     if q_type == 'open'
-      "<input type='text' name='#{name}' required>"
+      "<div class='form-group'><textarea name='#{name}' class='form-control' required></textarea></div>"
     elsif q_type == 'stars'
-      "<div class='c-rating'><input type='hidden' value=0 name='#{name}' required></div>"
+      "<div class='c-rating form-group'><input type='hidden' class='form-control' value=0 name='#{name}' required></div>"
     else
-      ""
+      ''
     end
   end
 end
