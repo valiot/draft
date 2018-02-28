002ff835de3cb23fa636384d626b04448d4a3665
gdavila09 <grrd99@gmail.com>
2018-02-28 13:15:12 -0600

    spacing in printers controller

--- app/controllers/printers_controller.rb
+++ app/controllers/printers_controller.rb
@@ -1,5 +1,7 @@
 class PrintersController < ApplicationController
+
   def print
-    @event =Event.find_by('starts_at > ? and ?< finishes_at', Date.today,Date.today)
-   end
+    @event = Event.find_by('starts_at > ? and ?< finishes_at', Date.today, Date.today)
+  end
+
 end
