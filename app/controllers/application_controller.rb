f0ad885d4e8fd0effd3510f5b259f81e797dcf84
gdavila09 <grrd99@gmail.com>
2018-02-28 12:58:43 -0600

    redirect only in event is on review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,13 +14,11 @@ class ApplicationController < ActionController::Base
     @invitation = @event&.invitations&.find_by(user: current_user, event: @event)
 
     # Existe el evento y ya tienes equipo
-    return redirect_to event_review_path(@event) if @event && @attendee&.team
+    return redirect_to event_review_path(@event) if @event && @attendee&.team && @event.review?
     # Existe el evento y ya hiciste checkin
     return redirect_to event_select_path(@event) if @event && @attendee
     # Existe el evento y ya aceptace la invitation
-    return redirect_to event_checkin_path(@event) if @event && @invitation&.accepted?
-    # Acceptaste invitacion y se envia a chekin
-    return redirect_to event_checkin_path(@event) if @event.checkin?
+    return redirect_to event_checkin_path(@event) if @event && @invitation&.accepted? && @event.checkin?
     # Existe el evento y la invitation esta pendiente
     return redirect_to invitations_path if @event && @invitation&.pending?
     redirect_to edit_user_path
